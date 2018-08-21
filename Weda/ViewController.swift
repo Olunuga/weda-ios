328aae7817924995ff3d2e042bd12c690ee9c5a1
Mayowa  Olunuga <myorh@myorhs-macbook.home>
2018-08-21 06:32:26 +0100

    Initial Commit

---
+++ Weda/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weda
+//
+//  Created by OLUNUGA Mayowa on 21/08/2018.
+//  Copyright © 2018 OLUNUGA Mayowa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
